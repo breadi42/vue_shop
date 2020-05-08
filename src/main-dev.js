523994856b2e63868c6b3de8e306187fc52e1513
李颍 <bestriven.928@qq.com>
2020-05-08 22:09:41 +0800

    项目优化

--- src/main-dev.js
+++ src/main-dev.js
@@ -8,20 +8,36 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 // 树形表格组件
 import TreeTable from 'vue-table-with-tree-grid'
+
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
 // 导入富文本编辑器对应的样式
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
+
+// 导入Nprogress进度条
+import NProgress from 'nprogress'
+// 导入对应的css样式表
+import 'nprogress/nprogress.css'
+
 // axios通信
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
 // axios请求拦截
+// 在request拦截器中显示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
+  // 在return之前展示进度条
+  NProgress.start()
   // 为请求头对象添加token验证的Authorization字段
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 在response拦截器中隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  // 在return之前隐藏进度条
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
